66e0544046c14775c26513768f1aaf9aedcf654a
luiggy macias <ferrinluiggy@gmail.com>
2022-02-01 10:36:03 -0500

    adding the read controller using postgres

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -75,6 +75,33 @@ const list: RequestHandler = async (_req, res) => {
   }
 };
 
+// const userById = async (
+//   req: RequestWithProfile,
+//   res: Response,
+//   next: NextFunction,
+//   id: any
+// ) => {
+//   try {
+//     const user = await User.findById(id); // find the user by id
+//     // console.log(user, "this is from userById");
+
+//     if (!user) {
+//       // if the user is not found
+//       return res.status(400).json({
+//         // return a status code of 400 and a message
+//         error: "User not found",
+//       });
+//     }
+//     req.profile = user; // add the user to the request object
+//     next();
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(400).json({
+//       error: dbErrorHandler.getErrorMessage(error) || error,
+//     });
+//   }
+// }; 61e17198d29ab06ac574166b
+
 const userById = async (
   req: RequestWithProfile,
   res: Response,
@@ -82,9 +109,12 @@ const userById = async (
   id: any
 ) => {
   try {
-    const user = await User.findById(id); // find the user by id
+    const { rows } = await pool.query("SELECT * FROM users WHERE _id = $1", [
+      id,
+    ]);
     // console.log(user, "this is from userById");
-
+    const user = rows[0];
+    console.log(user);
     if (!user) {
       // if the user is not found
       return res.status(400).json({
@@ -94,10 +124,10 @@ const userById = async (
     }
     req.profile = user; // add the user to the request object
     next();
-  } catch (error) {
+  } catch (error: any) {
     console.log(error);
     return res.status(400).json({
-      error: dbErrorHandler.getErrorMessage(error) || error,
+      error: error.message,
     });
   }
 };
@@ -110,6 +140,29 @@ const read = (req: RequestWithProfile, res: Response) => {
   return res.json(profile); // return the profile
 };
 
+// const update = async (req: RequestWithProfile, res: Response) => {
+//   try {
+//     let user = req.profile; // get the user from the request object
+//     const { body } = req; // get the body from the request object
+//     // update the user with the new values
+//     extend(user, body); // extend the user with the new values, if a value in body already exists, it will be overwritten in the user object
+//     // console.log(req.body);
+//     // user = Object.assign(user, body); // assign the new values to the user object
+//     // console.log(user);
+
+//     if (!user) return res.status(400).json({ error: "User not found" });
+//     user.updated = Date.now();
+//     const updatedUser = await user.save(); // this will only save the properties that are in the mongooose schema, so if you add a property c to a schema {a,b}
+//     updatedUser.hashed_password = undefined;
+//     // console.log(updatedUser);
+//     updatedUser.salt = undefined;
+//     return res.json(updatedUser);
+//   } catch (error) {
+//     return res.status(400).json({
+//       error: dbErrorHandler.getErrorMessage(error),
+//     });
+//   }
+// };
 const update = async (req: RequestWithProfile, res: Response) => {
   try {
     let user = req.profile; // get the user from the request object
@@ -133,7 +186,6 @@ const update = async (req: RequestWithProfile, res: Response) => {
     });
   }
 };
-
 const remove = async (req: RequestWithProfile, res: Response) => {
   try {
     let user = req.profile;
--- backend/src/types/RequestWithProfile.ts
+++ backend/src/types/RequestWithProfile.ts
@@ -1,12 +1,23 @@
 import { Request, NextFunction } from "express";
 import { IUserDoc } from "../models/user.model";
 
+// export interface RequestWithProfile extends Request {
+//   profile?:
+//     | (IUserDoc & {
+//         _id: any;
+//       })
+//     | null;
+// }
 export interface RequestWithProfile extends Request {
-  profile?:
-    | (IUserDoc & {
-        _id: any;
-      })
-    | null;
+  profile?: {
+    _id: string;
+    name: string;
+    email: string;
+    created: Date;
+    updated?: Date | number;
+    salt: string | undefined;
+    hashed_password: string | undefined;
+  } | null;
 }
 // export interface RequestHandlerWithProfile {
 //   (RequestWithProfile, Response, NextFunction, id_: any): Promise<void>;
