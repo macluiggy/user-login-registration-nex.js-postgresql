3104da02e68c0b9b026feeae4e224a799a325cc3
luiggy macias <ferrinluiggy@gmail.com>
2022-02-03 11:14:57 -0500

    djkd

--- backend/src/test/functional.test.js
+++ backend/src/test/functional.test.js
@@ -21,7 +21,23 @@ describe("Testing the api endpoints", () => {
           done();
         });
     });
-    it("Test POSTing a new user, Signing in and DELETing it", (done) => {
+    it("Should return a message error if one or more credential are missing when register", (done) => {
+      chai
+        .request(server)
+        .post(apiUsers)
+        .send({
+          name: "",
+          email: "",
+          password: "",
+        })
+        .end((err, res) => {
+          console.log(res.body);
+          assert.equal(res.status, 400);
+          assert.equal(res.body.message, "Missing credentials");
+          done();
+        });
+    });
+    it("Test creating ", (done) => {
       chai
         .request(server)
         .post(apiUsers)
@@ -40,15 +56,16 @@ describe("Testing the api endpoints", () => {
             .post("/auth/signin")
             .send({ email: "testing@gmail.com", password: "testing" })
             .end((err, res) => {
-              console.log(res.body, "signing in");
-
+              // console.log(res.body, "signing in");
+              assert.equal(res.status, 200);
+              assert.isObject(res.body);
               //getting the user
               chai
                 .request(server)
                 .get(`/api/users/${res.body.user._id}`)
                 .set("Authorization", `Bearer ${res.body.token}`)
                 .end((err, res) => {
-                  console.log(res.body, "getting the user");
+                  // console.log(res.body, "getting the user");
                 });
               // updating the user
               chai
@@ -61,7 +78,7 @@ describe("Testing the api endpoints", () => {
                   password: "testing",
                 })
                 .end((err, res) => {
-                  console.log(res.body, "updating the user");
+                  // console.log(res.body, "updating the user");
                 });
               //deleting the user
               chai
@@ -69,7 +86,7 @@ describe("Testing the api endpoints", () => {
                 .delete(apiUsers + "/" + res.body.user._id)
                 .set("Authorization", "Bearer " + res.body.token)
                 .end((err, res) => {
-                  console.log(res.body, "DELETING THE USER ");
+                  // console.log(res.body, "DELETING THE USER ");
                 });
             });
 
