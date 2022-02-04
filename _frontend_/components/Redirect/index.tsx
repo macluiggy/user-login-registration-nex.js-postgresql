115db78b325d4d73576659692c65646129ef2434
luiggy macias <ferrinluiggy@gmail.com>
2022-02-04 14:09:16 -0500

    dddd

--- _frontend_/components/Redirect/index.tsx
+++
@@ -1,6 +0,0 @@
-import { useRouter } from "next/router";
-export default function Redirect({ path }) {
-  const router = useRouter();
-  router.push(path || "/");
-  return <div>redirecting</div>;
-}
